110f62c77c392a943f03dbe5d03cb9f4f485674e
Admin <Admin@DESKTOP-S5DQKIR>
2026-02-04 17:20:12 +0100

    Creating app.config and routes and adjusting the links

--- frontend/footballWebApp/src/app/app.routes.ts
+++ frontend/footballWebApp/src/app/app.routes.ts
@@ -1,3 +1,11 @@
 import { Routes } from '@angular/router';
+import { PlayersComponent } from './components/players.component/players.component';
+import { MatchesComponent } from './components/matches.component/matches.component';
+import { HomepageComponent } from './components/homepage.component/homepage.component';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+    {path: '', redirectTo: 'homepage', pathMatch: 'full'},
+    {path: 'players', component: PlayersComponent},
+    {path: 'matches', component: MatchesComponent},
+    {path: 'homepage', component: HomepageComponent}
+];
