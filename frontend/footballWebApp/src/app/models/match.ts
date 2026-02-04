f3e6f61c699a23105be4f9db47ad51a44a043ee3
Admin <Admin@DESKTOP-S5DQKIR>
2026-02-04 17:20:47 +0100

    implementing the model interfaces for angular web app (player and match)

---
+++ frontend/footballWebApp/src/app/models/match.ts
@@ -0,0 +1,8 @@
+export interface Match {
+ matchId?: number,
+ opponent: string,
+ matchDate: Date,
+ location: string,
+ goalsFor:number,
+ goalsAgainst: number
+};
---
+++ frontend/footballWebApp/src/app/models/player.ts
@@ -0,0 +1,9 @@
+export interface Player {
+  id?: number;
+  firstName: string;
+  lastName: string;
+  position: string;
+  shirtNumber: number;
+  nationality: string;
+  isActive: boolean;
+}
