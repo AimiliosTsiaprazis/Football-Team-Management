d8ab549e2a75706e892ab59c18b050042eeb6d6f
Admin <Admin@DESKTOP-S5DQKIR>
2026-02-04 23:31:04 +0100

    Debugging

--- frontend/footballWebApp/karma.conf.js
+++ frontend/footballWebApp/karma.conf.js
@@ -9,16 +9,11 @@ module.exports = function (config) {
       require('karma-coverage'),
       require('@angular-devkit/build-angular/plugins/karma')
     ],
-    client: {
-      clearContext: false // leave Jasmine Spec Runner output visible in browser
-    },
+    client: { clearContext: false },
     coverageReporter: {
       dir: require('path').join(__dirname, './coverage'),
       subdir: '.',
-      reporters: [
-        { type: 'html' },
-        { type: 'text-summary' }
-      ]
+      reporters: [{ type: 'html' }, { type: 'text-summary' }]
     },
     reporters: ['progress', 'kjhtml'],
     port: 9876,
